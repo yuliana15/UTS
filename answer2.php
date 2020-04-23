9e6a283a205e932ca9b78625eed43d7d968a9d52
unknown <ulieana6@gmail.com>
2020-04-23 19:34:13 +0700

    answer2

---
+++ answer2.php
@@ -0,0 +1,12 @@
+<h1> Variable content </h1>
+
+<?php
+$nama= 'Siti Yuliana';
+?>Nama Saya : <?php echo $nama;?> <br>
+Variabel nama Saya : <?php var_dump($nama);?>
+<br>
+<?php
+$umur= '26';
+?>Umur Saya : <?php echo $umur;?> <br>
+Variabel umur Saya : <?php var_dump($umur);?>
+
